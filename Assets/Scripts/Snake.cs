2437b553e736e586439b7dab6790481b8cbcbd44
mmarcod <marco.davila001@mymdc.net>
2024-11-24 16:43:16 -0500

    Another Push

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Snake : MonoBehaviour
+{
+    Vector2 dir = Vector2.right;
+
+    List<Transform> tail = new List<Transform>();
+
+    bool ate = false;
+
+    public GameObject tailPrefab;
+
+    private void Start()
+    {
+        InvokeRepeating("Move", 0.3f, 0.3f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = -Vector2.up;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = -Vector2.right;
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector2.up;
+    }
+    void Move()
+    {
+        Vector2 v = transform.position;
+
+        transform.Translate(dir);
+
+        if (ate)
+        {
+            GameObject g = (GameObject)Instantiate(tailPrefab, v, Quaternion.identity);
+
+            tail.Insert(0, g.transform);
+
+            ate = false;
+        }
+        else if (tail.Count > 0)
+        {
+
+            tail.Last().position = v;
+
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.name.StartsWith("Food"))
+        {
+            ate = true;
+
+            Destroy(coll.gameObject);
+        }
+        else
+        {
+
+        }
+    }
+}
